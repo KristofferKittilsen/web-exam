46f84eb396ee51588a592f64788a439a5dbd8a6b
KristofferKittilsen <42611647+KristofferKittilsen@users.noreply.github.com>
2019-05-27 13:46:18 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,19 @@
 var cal = {
-  /* [PROPERTIES] */
   mName : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], // Month Names
-  data : null, // Events for the selected period
-  sDay : 0, // Current selected day
-  sMth : 0, // Current selected month
-  sYear : 0, // Current selected year
-  sMon : false, // Week start on Monday?
+  data : null, 
+  sDay : 0, 
+  sMth : 0, 
+  sYear : 0, 
+  sMon : false, 
 
-  /* [FUNCTIONS] */
   list : function () {
-  // cal.list() : draw the calendar for the given month
+  
+    cal.sMth = parseInt(document.getElementById("cal-mth").value); 
+    cal.sYear = parseInt(document.getElementById("cal-yr").value); 
+    var daysInMth = new Date(cal.sYear, cal.sMth+1, 0).getDate(), 
+        startDay = new Date(cal.sYear, cal.sMth, 1).getDay(), 
+        endDay = new Date(cal.sYear, cal.sMth, daysInMth).getDay(); 
 
-    // BASIC CALCULATIONS
-    // Note - Jan is 0 & Dec is 11 in JS.
-    // Note - Sun is 0 & Sat is 6
-    cal.sMth = parseInt(document.getElementById("cal-mth").value); // selected month
-    cal.sYear = parseInt(document.getElementById("cal-yr").value); // selected year
-    var daysInMth = new Date(cal.sYear, cal.sMth+1, 0).getDate(), // number of days in selected month
-        startDay = new Date(cal.sYear, cal.sMth, 1).getDay(), // first day of the month
-        endDay = new Date(cal.sYear, cal.sMth, daysInMth).getDay(); // last day of the month
-
-    // LOAD DATA FROM LOCALSTORAGE
     cal.data = localStorage.getItem("cal-" + cal.sMth + "-" + cal.sYear);
     if (cal.data==null) {
       localStorage.setItem("cal-" + cal.sMth + "-" + cal.sYear, "{}");
@@ -29,8 +22,6 @@ var cal = {
       cal.data = JSON.parse(cal.data);
     }
 
-    // DRAWING CALCULATIONS
-    // Determine the number of blank squares before start of month
     var squares = [];
     if (cal.sMon && startDay != 1) {
       var blanks = startDay==0 ? 7 : startDay ;
@@ -40,10 +31,8 @@ var cal = {
       for (var i=0; i<startDay; i++) { squares.push("b"); }
     }
 
-    // Populate the days of the month
     for (var i=1; i<=daysInMth; i++) { squares.push(i); }
 
-    // Determine the number of blank squares after end of month
     if (cal.sMon && endDay != 0) {
       var blanks = endDay==6 ? 1 : 7-endDay;
       for (var i=0; i<blanks; i++) { squares.push("b"); }
@@ -53,15 +42,12 @@ var cal = {
       for (var i=0; i<blanks; i++) { squares.push("b"); }
     }
 
-    // DRAW HTML
-    // Container & Table
     var container = document.getElementById("cal-container"),
         cTable = document.createElement("table");
     cTable.id = "calendar";
     container.innerHTML = "";
     container.appendChild(cTable);
 
-    // First row - Days
     var cRow = document.createElement("tr"),
         cCell = null,
         days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
@@ -74,7 +60,6 @@ var cal = {
     cRow.classList.add("head");
     cTable.appendChild(cRow);
 
-    // Days in Month
     var total = squares.length;
     cRow = document.createElement("tr");
     cRow.classList.add("day");
@@ -98,18 +83,13 @@ var cal = {
       }
     }
 
-    // REMOVE ANY ADD/EDIT EVENT DOCKET
     cal.close();
   },
 
   show : function (el) {
-  // cal.show() : show edit event docket for selected day
-  // PARAM el : Reference back to cell clicked
-
-    // FETCH EXISTING DATA
+    
     cal.sDay = el.getElementsByClassName("dd")[0].innerHTML;
 
-    // DRAW FORM
     var tForm = "<h1>" + (cal.data[cal.sDay] ? "EDIT" : "ADD") + " EVENT</h1>";
     tForm += "<div id='evt-date'>" + cal.sDay + " " + cal.mName[cal.sMth] + " " + cal.sYear + "</div>";
     tForm += "<textarea id='evt-details' required>" + (cal.data[cal.sDay] ? cal.data[cal.sDay] : "") + "</textarea>";
@@ -117,7 +97,6 @@ var cal = {
     tForm += "<input type='button' value='Delete' onclick='cal.del()'/>";
     tForm += "<input type='submit' value='Save'/>";
 
-    // ATTACH
     var eForm = document.createElement("form");
     eForm.addEventListener("submit", cal.save);
     eForm.innerHTML = tForm;
@@ -127,13 +106,11 @@ var cal = {
   },
 
   close : function () {
-  // cal.close() : close event docket
 
     document.getElementById("cal-event").innerHTML = "";
   },
 
   save : function (evt) {
-  // cal.save() : save event
 
     evt.stopPropagation();
     evt.preventDefault();
@@ -143,7 +120,6 @@ var cal = {
   },
 
   del : function () {
-  // cal.del() : Delete event for selected date
 
     if (confirm("Remove event?")) {
       delete cal.data[cal.sDay];
@@ -153,14 +129,12 @@ var cal = {
   }
 };
 
-// INIT - DRAW MONTH & YEAR SELECTOR
 window.addEventListener("load", function () {
-  // DATE NOW
+
   var now = new Date(),
       nowMth = now.getMonth(),
       nowYear = parseInt(now.getFullYear());
 
-  // APPEND MONTHS SELECTOR
   var month = document.getElementById("cal-mth");
   for (var i = 0; i < 12; i++) {
     var opt = document.createElement("option");
@@ -170,8 +144,6 @@ window.addEventListener("load", function () {
     month.appendChild(opt);
   }
 
-  // APPEND YEARS SELECTOR
-  // Set to 10 years range. Change this as you like.
   var year = document.getElementById("cal-yr");
   for (var i = nowYear-10; i<=nowYear+10; i++) {
     var opt = document.createElement("option");
@@ -181,7 +153,6 @@ window.addEventListener("load", function () {
     year.appendChild(opt);
   }
 
-  // START - DRAW CALENDAR
   document.getElementById("cal-set").addEventListener("click", cal.list);
   cal.list();
 });
