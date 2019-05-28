2bfa9565724ed0145fd0841c4e46c689c3073ae3
Kristoffer <kristoffer-2000@hotmail.com>
2019-05-28 13:30:32 +0200

    Updating everything

--- js/dragAndDropBoard.js
+++
@@ -1,7 +0,0 @@
-$('#item1, #item2').bind('dragstart', function(event) {
-    event.originalEevent.dataTransfer.setData("text/plain", event.target.getAttribute('id'));
-});
-
-$('#todo, #inprogress, #done').bind('dragover', function(event){
-    event.preventDefault();
-});
