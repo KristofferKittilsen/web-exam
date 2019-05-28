2c8d59873766d1600b4596a2c792acfb8bf59230
Kristoffer <kristoffer-2000@hotmail.com>
2019-05-28 13:38:19 +0200

    making more things

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 function faqalert() {
     alert("No one reads this anyway!!");
 }
 function contactalert() {
     alert("Contact information: test@test.com " + "\n" + "Phonenumber: 28119823")
 }
-=======
->>>>>>> ed1a2443bf8fbb2d8796027959e582c9cadfd67d
+
