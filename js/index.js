4ac2c3f4ece0655d78bbc93b5503a1f246e7efc6
Kristoffer <kristoffer-2000@hotmail.com>
2019-05-31 12:22:44 +0200

    Updating kanban site

--- js/index.js
+++ js/index.js
@@ -1,14 +1,5 @@
-function faqalert() {
-    alert("No one reads this anyway!!");
-}
-
 function contactalert() {
     alert("Contact information: test@test.com " + "\n" + "Phonenumber: 28119823")
-}
-
-function inputPrompt() {
-    document.getElementById("inputBox").style.display = "block";
-
 }
 
 const landingdiv = document.getElementById("item1");
@@ -22,6 +13,10 @@ function createDiv() {
 
 
     var div = document.createElement("div");
+    div.setAttribute("class", "kanbanPage");
+    div.setAttribute("draggable", true);
+    div.setAttribute("ondragstart", "event.dataTransfer.setData('text/plain', null)");
+
 
     var h1 = document.createElement("h1");
     h1.innerText = title;
@@ -39,18 +34,52 @@ newBtn.addEventListener("click", function () {
     createDiv();
 })
 
+var dragged;
 
-var item1 = document.querySelector("item1");
+document.addEventListener("drag", function (event) {
 
-function dragginStarts(e) {
-    item1 = e.target;
-    item1 = style.opacity = "0.2";
+}, false);
+
+document.addEventListener("dragstart", function (event) {
+    dragged = event.target;
+    event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function (event) {
+    event.target.style.opacity = "";
+}, false);
+
+document.addEventListener("dragover", function (event) {
+    event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function (event) {
+    if (event.target.classname == "column") {
+        event.target.style.background = "purple";
+    }
+}, false);
+
+document.addEventListener("dragleave", function (event) {
+    if (event.target.className == "column") {
+        event.target.style.background = "";
+    }
+}, false);
+
+document.addEventListener("drop", function (event) {
+    event.preventDefault();
+    if (event.target.className == "column") {
+        dragged.parentNode.removeChild(dragged);
+        event.target.appendChild(dragged);
+    }
+}, false);
+
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
 }
 
-item1.addEventListener("dragstart", draggingStarts, false);
-
-
-
-
-
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
 
