8b1c3917e76681b975edc907a993a05035d4afd6
Kristoffer <kristoffer-2000@hotmail.com>
2019-05-31 12:30:04 +0200

    Fixed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ function contactalert() {
     alert("Contact information: test@test.com " + "\n" + "Phonenumber: 28119823")
 }
 
-<<<<<<< HEAD
 const landingdiv = document.getElementById("item1");
 const newBtn = document.getElementById("newBtn");
 
@@ -12,48 +11,8 @@ function createDiv() {
     const title = document.getElementById("title").value;
     const text = document.getElementById("text").value;
 
-=======
-<<<<<<< HEAD
+   
 
-$(onPageLoad);
-
-function onPageLoad() {
-    $(".column").sortable({
-        connectWith: ".column",
-        handle: ".portlet-header",
-        cancel: ".portlet-toggle",
-        start: function (event, ui) {
-            ui.item.addClass('tilt');
-        },
-        stop: function (event, ui) {
-            ui.item.removeClass('tilt');
-        }
-    });
-
-    $(".portlet")
-        .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
-        .find(".portlet-header")
-        .addClass("ui-widget-header ui-corner-all")
-        .prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
-
-    $(".portlet-toggle").click(function () {
-        var icon = $(this);
-        icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
-        icon.closest(".portlet").find(".portlet-content").toggle();
-    });
-}
-
-
-=======
->>>>>>> 86825b5bbc083f2b39c5e3203f142811d8734489
-function inputPrompt() {
-    document.getElementById("inputBox").style.display = "block";
-
-}
-<<<<<<< HEAD
-
-=======
->>>>>>> 4761e2050808bd382c47dd8dc13d926752ed1148
 
     var div = document.createElement("div");
     div.setAttribute("class", "kanbanPage");
@@ -125,9 +84,3 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
-
-<<<<<<< HEAD
-=======
-} */
->>>>>>> 86825b5bbc083f2b39c5e3203f142811d8734489
->>>>>>> 4761e2050808bd382c47dd8dc13d926752ed1148
