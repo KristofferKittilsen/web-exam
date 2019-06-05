b913e0ceb5932e8e990210da29de81fb590f9f75
Micah Wonder <wonderkid0198@gmail.com>
2019-06-05 12:45:17 +0200

    mkm

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
 function check(form) {
     if (form.userid.value == "test" && form.pswrd.value == "test") {
         window.open(href = 'index.html')
